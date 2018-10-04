57f20e67bc01d332ff69ae499235575915adeeef
Noah Garfinkle <noah.garfinkle@gmail.com>
2018-10-04 14:05:58 -0500

    tested some simple database and admin page additions

--- toytaco/datamanagement/admin.py
+++ toytaco/datamanagement/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Asset, Asset_Type
 
 # Register your models here.
+admin.site.register(Asset)
+admin.site.register(Asset_Type)
--- toytaco/datamanagement/models.py
+++ toytaco/datamanagement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Asset_Type(models.Model):
+    name = models.CharField(max_length=50)
+
+class Asset(models.Model):
+    pretty_name = models.CharField(max_length=50)
